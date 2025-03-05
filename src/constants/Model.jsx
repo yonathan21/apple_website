e7898d3585610b47499aaae64ce51fb1805b7587
yonathan21 <eldanadereje06@gmail.com>
2025-03-06 02:11:40 +0300

    doingsizebuttons

--- src/constants/Model.jsx
+++ src/constants/Model.jsx
@@ -6,6 +6,7 @@ import { yellowImg } from "../utils";
 import * as THREE from 'three';
 import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
+import { models, sizes } from ".";
 
 const Model = () => {
   const [size, setSize] = useState('small');
@@ -76,6 +77,30 @@ const Model = () => {
             >
               <View.Port />
             </Canvas>
+          </div>
+
+          <div className="mx-auto w-full">
+            <p className="text-sm font-light text-center mb-5">{model.title}</p>
+            <div className="flex-center">
+              <ul className="color-container">
+                {models.map((item,i) => (
+                  <li key={i} className="w-6 h-6 rounded-full mx-2 cursor-pointer" style={{
+                    backgroundColor: item.color[0]
+                  }}
+                  onClick={()=>setModel(item)}/>
+                ))}
+              </ul>
+
+              <button className="size-btn-container">
+                {sizes.map(({label, value}) => (
+                  <span key={label} className="size-btn"
+                    style={{backgroundColor: size === value ? 'white': 'transparent', color: size === value ? 'black' : 'white'}} onClick={() => setSize(value)}
+                  >
+                    {label}
+                  </span>
+                ))}
+              </button>
+            </div>
           </div>
         </div>
       </div>
